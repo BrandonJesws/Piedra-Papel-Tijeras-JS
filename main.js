4ca7e33e8d3f01af193e226333d6a718bde13e12
BrandonJesws <jesusw52@gmail.com>
2019-06-04 10:17:18 -0500

    marcador y texto

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const compuScore = 0;
+var userScore = 0;
+var compuScore = 0;
 const userScore_span = document.querySelector("#user-score");
 const compuScore_span = document.querySelector("#comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result p");
 const piedra_div = document.querySelector("#r");
 const papel_div = document.querySelector("#p");
 const tijeras_div = document.querySelector("#s");
@@ -13,6 +13,27 @@ function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
+function convertWord(letter){
+    if(letter === "r") return "Piedra";
+    if(letter === "p") return "Papel";
+    if(letter === "s") return "Tijeras";
+}
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result_p.innerHTML = `${convertWord(userChoice)} es mejor que ${convertWord(computerChoice)}. 
+                            <br/>¡Tu Ganas!"`;
+}
+function draw(userChoice, computerChoice){
+    result_p.innerHTML = `${convertWord(userChoice)} y ${convertWord(computerChoice)} son iguales. 
+                            <br/>¡Empate!"`;
+}
+function lose(userChoice, computerChoice){
+    compuScore++;
+    compuScore_span.innerHTML = compuScore;
+    result_p.innerHTML = `${convertWord(userChoice)} no es mejor que ${convertWord(computerChoice)}. 
+                            <br/>¡Tu Pierdes!"`;
+}
 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
@@ -20,17 +41,17 @@ function game(userChoice) {
         case "rs":
         case "pr":
         case "sp":
-            console.log("Tu Ganas");
+            win(userChoice, computerChoice);
             break;
         case "rr":
         case "ss":
         case "pp":
-            console.log("Empate");
+            draw(userChoice, computerChoice);
             break;
         case "ps":
         case "sr":
         case "rp":
-            console.log("Tu pierdes");
+            lose(userChoice, computerChoice);
             break;
         default:
             console.log("Error");
